ecd0e6fda8ef241a45064cc5ace1b4bce9a170f8
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-07-22 15:38:28 +1200

    code to generate results table is now defined elsewhere

---
+++ beta_distribution/fit_zero_inflated_beta_model.R
@@ -0,0 +1,36 @@
+####
+# code that fits all candidate models to an example dataset and generates a model comparison table
+####
+
+# read in the model-fitting functions
+source('brmsBetaFunctions.R')
+
+# read in the post-processing functions
+source('resultsFunctions.R')
+
+# load the sample data frame
+example_data <- readRDS('../example.Rdata')
+
+# define the different models which are to be fit to the data
+models <- c(
+  "Null",
+  "Ambient",
+  "Warming",
+  "Removal",
+  "Removal_+_Warming",
+  "Removal_x_Warming"
+)
+
+# fit each of the above specified models to the data
+model.fits <- sapply(
+  models,
+  function(model.name,df){
+    fit <- model.fit.beta(df, model.name)
+    return(fit)
+  },
+  df = example_data,
+  simplify = FALSE
+)
+
+# generate a pretty table of model comparison statistics
+results <- results.table(model.fits)
