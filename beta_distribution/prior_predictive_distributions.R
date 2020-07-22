44f5859bec4146c83242410592d17079297cc6f4
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-07-22 15:06:08 +1200

    this code only samples from the prior to show that the data-uninformed starting point is a biological reasonable generative model

---
+++ beta_distribution/prior_predictive_distributions.R
@@ -0,0 +1,42 @@
+####
+# code that samples from the posterior to demonstrate plausibility of models when not fit to data
+####
+
+# model fits are performed with brms
+library(brms)
+
+# allow for parallelization of MCMC chains
+options(mc.cores = parallel::detectCores())
+
+# read in the model-fitting functions
+source('brmsBetaFunctions.R')
+
+# load the sample data frame
+example_data <- readRDS('../example.Rdata')
+
+# define the different models which are to be fit to the data
+models <- c(
+  "Null",
+  # "Ambient",
+  # "Warming",
+  # "Removal",
+  # "Removal_+_Warming",
+  "Removal_x_Warming"
+)
+
+# generate samples purely from the prior to visualize prior predictions
+model.fits <- sapply(
+  models,
+  function(model.name,df){
+    fit <- model.fit.beta(df, model.name)
+    return(fit)
+  },
+  df = example_data,
+  simplify = FALSE
+)
+
+# use conditional effects to plot prior predictive distributions for the null model
+conditional_effects(model.fits[["Null"]])
+
+# use conditional effects to plot prior predictive distributions for the full model
+conditional_effects(model.fits[["Removal_x_Warming"]])
